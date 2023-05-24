b1dcd26839cdad8fa6851c2c0cb720ae16a84984
Thomas Oggier <thomas.oggier@soprod.ch>
2023-05-24 14:54:48 +0200

    get information + configuration

--- AlphaInnotec/LWCV.js
+++ AlphaInnotec/LWCV.js
@@ -25,14 +25,6 @@ module.exports = function (RED) {
             console.log("Try to connect socket");
             var socket = new ws(node.path, node.subprotocol, options);
             node.socket = socket;
-    
-            socket.on('open', function () {
-                console.log("Socket Opened");
-                // Connect wih the password
-                socket.send("LOGIN;"+310790);
-                console.log("Done");
-            });
-
             socket.on('error', function (err) {
                 console.log("Socket Error");
                 console.log(err);
@@ -42,21 +34,30 @@ module.exports = function (RED) {
                 console.log("Socket closed");
                 connectSocket();
             });
-            socket.on('message', function (data, flags) {
-                console.log("Get message");
-                var datastring = data.toString();
-                const options = {   ignoreAttributes : false    };
-                const parser = new XMLParser(options);
-                let jsonObj = parser.parse(datastring); 
-                //console.log(jsonObj);
-                console.log(jsonObj['Navigation']);
-                const { item } = jsonObj['Navigation'];
-                item.forEach(getItemIdForNavigation);
-                
-            });
             //socket.on('ping', function () { console.log("ping"); });
             //socket.on('pong', function () { console.log("pong"); });
+            socket.on('open', function () {
+                console.log("Socket Opened");
+                // Connect wih the password
+                socket.send("LOGIN;"+310790);
+                console.log("Done");
+                socket.once('message', handlerNavigationRequest);
+            });
         };
+// Navigations
+        function handlerNavigationRequest(data) {
+            console.log("handlerNavigationRequest");
+            var datastring = data.toString();
+            const options = {   ignoreAttributes : false    };
+            const parser = new XMLParser(options);
+            let jsonObj = parser.parse(datastring); 
+            //console.log(jsonObj);
+            //console.log(jsonObj['Navigation']);
+            const { item } = jsonObj['Navigation'];
+            item.forEach(getItemIdForNavigation);
+            node.socket.once('message', handlerInformationsRequest);
+            node.socket.send("GET;"+node.informationsId);
+        }
         function getItemIdForNavigation(item)
         {
             switch (item['name'])
@@ -83,14 +84,87 @@ module.exports = function (RED) {
                     break;
                 }
         }
-        // On msg
-        node.on('input', function(msg) {
-            switch (msg.payload) { 
+// Informations
+        function handlerInformationsRequest(data) {
+            console.log("handlerInformationsRequest");
+            shortResult(data);
+            node.socket.once('message', handlerConfigurationRequest);
+            node.socket.send("GET;"+node.configurationId);
+        }
+// Configuration
+        function handlerConfigurationRequest(data) {
+            console.log("handlerConfigurationRequest");
+            shortResult(data);
+            //node.socket.once('message', handlerInformationsRequest);
+            //node.socket.send("GET;"+node.configurationId);
+        }
+// Common Result
+        function shortResult(data) {
+            var datastring = data.toString();
+            const options = {   ignoreAttributes : false    };
+            const parser = new XMLParser(options);
+            let jsonObj = parser.parse(datastring);
+            const { item } = jsonObj['Content'];
+
+            if (Array.isArray(item))
+                item.forEach(function (elementgroup) {
+                    console.log("============================");
+                    //console.log(elementgroup);
+                    const { item } = elementgroup;
+                    const groupName = elementgroup['name'][0];
+                    if (Array.isArray(item))
+                        item.forEach(function (element) { 
+                            sendResultValue(groupName, element['name'], element['value']);
+                        })
+                    else
+                        sendResultValue(groupName, item['name'], item['value']);                    
+                });
+        }
+
+// Result
+        function getValueAndUnit(value)
+        {
+            var unitsToRemove = ["kWh", "°C", "bar", "l/h", "%", "h", "RPM", "V", "K"];
+            var unit;
+            for (var unit_pos in unitsToRemove)
+            {
+                value = value.toString();
+                if(value.includes(unitsToRemove[unit_pos]))
+                {
+                    value = value.replace(unitsToRemove[unit_pos], '');
+                    unit = unitsToRemove[unit_pos];
+                }
+            }
+            value.trim();
+            return [value, unit];
+        }
+        function sendResultValue(group, name, value) {
+            var msg = {};
+            var valueWithUnits = getValueAndUnit(value)
+            msg.payload = {
+                'group': group,
+                'name': name,
+                'value': valueWithUnits[0],
+                'units': valueWithUnits[1]
+            }
+            console.log(group + ": " + name + " -> " + valueWithUnits[0] +"["+valueWithUnits[1]+"]");
+            node.send(msg);
+        }
+ // node.on msg
+        node.on('input', function(msg, send, done) {
+            switch (msg.payload.function) { 
                 case "refresh":
-                    node.socket.ping();
+                    node.socket.once('message', handlerInformationsRequest);
+                    node.socket.send("GET;"+node.informationsId);
+                    break;
+                case "ecs":
+                    node.socket.send("SET;set_"+node.ecsId+msg.mode);
+                    break;
+                case "heating":
+                    node.socket.send("SET;set_"+node.heatingId+msg.mode);
                     break;
             }
-            node.send(msg);
+            done();
         });
     }
     RED.nodes.registerType("LWCV",LWCV);
